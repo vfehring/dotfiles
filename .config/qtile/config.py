4a129fc21024f6d8aed172a188d163531b92982e
Vincenzo Fehring <vinfehring@proton.me>
2024-02-03 01:46:16 -0600

    Added keybind to qtile to maximize windows

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -78,6 +78,7 @@ keys = [
     Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
     Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
+    Key([mod], "m", lazy.window.toggle_maximize(), desc="Toggle maximize"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
     # Toggle between split and unsplit sides of stack.
     # Split = all windows displayed
